2a153c49d887d8125a20f1d2c7f85955dffa4c4a
Mauricio Desktop <prussdesign@gmail.com>
2019-09-07 16:15:57 -0300

    Passando parâmetros para o controller

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping("/")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá usuário REST Spring Boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam(value="nome", defaultValue = "Usuário") String nome) {
+		System.out.println("Parametro recebido: " + nome);
+		return new ResponseEntity("Olá " + nome + ". Primeira aula de REST Spring Boot", HttpStatus.OK);
 	}
 	
 }
