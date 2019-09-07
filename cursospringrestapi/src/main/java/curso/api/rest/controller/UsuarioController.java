980988eab83a727bcc5268737fe88a5029bdab05
Mauricio Desktop <prussdesign@gmail.com>
2019-09-07 18:33:26 -0300

    Adicionado novas funções no controller do usuario

--- cursospringrestapi/src/main/java/curso/api/rest/controller/UsuarioController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/UsuarioController.java
@@ -3,8 +3,12 @@ package curso.api.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,14 +31,32 @@ public class UsuarioController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> init(@PathVariable("id") Long id) {
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable("id") Long id) {
 		Usuario usuario = usuarioRepository.findById(id).get();
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Usuario> deletarPorId(@PathVariable("id") Long id) {
+		usuarioRepository.deleteById(id);
+		return new ResponseEntity<Usuario>(HttpStatus.OK);
+	}
+	
 	@GetMapping("/")
-	public ResponseEntity<Iterable<Usuario>> init() {
+	public ResponseEntity<Iterable<Usuario>> listarTodos() {
 		return new ResponseEntity<Iterable<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	@PutMapping("/")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		Usuario usuarioAtualizado = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioAtualizado, HttpStatus.OK);
 	}
 	
 }
