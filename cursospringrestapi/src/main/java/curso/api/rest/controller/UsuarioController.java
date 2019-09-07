18c9b9f30819ce5d1cdab7e34f23364bc83c9639
Mauricio Desktop <prussdesign@gmail.com>
2019-09-07 16:55:33 -0300

    Adicionado buscar todos e buscar por id

---
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/UsuarioController.java
@@ -0,0 +1,32 @@
+package curso.api.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> init(@PathVariable("id") Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<Iterable<Usuario>> init() {
+		return new ResponseEntity<Iterable<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+	}
+	
+}
